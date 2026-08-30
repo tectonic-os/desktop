aaa18693b9fddacf2e69b3a13e846667b6a4ce7f
John Grant <john@johngrant.dev>
2026-08-30 21:23:38 +1000

    roll to tect 0.6.11 and the collection's swapped asset placement

--- generated/desktop.d/tectonic-os/tect.sh
+++ generated/desktop.d/tectonic-os/tect.sh
@@ -5,8 +5,8 @@ set -euxo pipefail
 MODDIR=/ctx/modules/.remote/tectonic-os/tect
 export MODDIR
 
-export ASSET_TECT_VERSION="0.6.10"
-export ASSET_TECT_URL="https://github.com/tectonic-os/tectonic/releases/download/v0.6.10/tect-v0.6.10-x86_64-linux-musl.tar.gz"
-export ASSET_TECT_SHA256="b8f29f839a2dd46f57c4cac1a0e9a22924cd628eb572083386d2c326b22c8f3f"
+export ASSET_TECT_VERSION="0.6.11"
+export ASSET_TECT_URL="https://github.com/tectonic-os/tectonic/releases/download/v0.6.11/tect-v0.6.11-x86_64-linux-musl.tar.gz"
+export ASSET_TECT_SHA256="96d85fe69d9e0ef03da4d71ca7dd7debdca61a66b2b000fc91bb0f8b2fa9ce79"
 
 source /ctx/modules/.remote/tectonic-os/tect/module.sh
